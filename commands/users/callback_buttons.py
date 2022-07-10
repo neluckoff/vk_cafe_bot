c9c29f5f001cca570d6c7ff90e2743895384a060
Dmitry Luckyanenko <neluckoff@gmail.com>
2022-07-10 18:36:44 +0300

    Update callback_buttons.py

--- commands/users/callback_buttons.py
+++ commands/users/callback_buttons.py
@@ -11,21 +11,19 @@ async def product_handler(event: GroupTypes.MessageEvent):
             event_id=event.object.event_id,
             peer_id=event.object.peer_id,
             user_id=event.object.user_id,
-            event_data=OpenLinkEvent(link="https://vk.com/market-201079043").json()
+            event_data=OpenLinkEvent(link="URL").json()
         )
     elif event.object.payload["cmd"] == "reviews":
         await vk.api.messages.send_message_event_answer(
             event_id=event.object.event_id,
             peer_id=event.object.peer_id,
             user_id=event.object.user_id,
-            event_data=OpenLinkEvent(link="https://yandex.ru/maps/org/"
-                                          "osetinskiye_pirogi_alaniya/76917250814/"
-                                          "reviews/?ll=37.476872%2C56.006735&z=17").json()
+            event_data=OpenLinkEvent(link="URL").json()
         )
     elif event.object.payload["cmd"] == "sales":
         await vk.api.messages.send_message_event_answer(
             event_id=event.object.event_id,
             peer_id=event.object.peer_id,
             user_id=event.object.user_id,
-            event_data=OpenLinkEvent(link="https://vk.com/pirogi_alania?w=app5898182_-201079043").json()
+            event_data=OpenLinkEvent(link="URL").json()
         )
